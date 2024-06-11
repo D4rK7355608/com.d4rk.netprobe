22eac96036a8169b26b63c0f8f76fde54b00e5ac
D4rK7355608 <d4rk7355608@gmail.com>
2024-06-11 11:19:07 +0300

    Modified AD ID constants to match app usage

--- app/src/main/kotlin/com/d4rk/netprobe/constants/ads/AdsConstants.kt
+++ app/src/main/kotlin/com/d4rk/netprobe/constants/ads/AdsConstants.kt
@@ -1,9 +1,9 @@
 package com.d4rk.netprobe.constants.ads
 
 object AdsConstants {
-    const val BANNER_AD_UNIT_ID = "ca-app-pub-5294151573817700/8040893463"
+    const val BANNER_AD_UNIT_ID = "ca-app-pub-5294151573817700/6563108883"
     const val BANNER_AD_UNIT_ID_TEST = "ca-app-pub-3940256099942544/6300978111"
 
-    const val APP_OPEN_UNIT_ID = "ca-app-pub-5294151573817700/9208287867"
+    const val APP_OPEN_UNIT_ID = "ca-app-pub-5294151573817700/6870214737"
     const val APP_OPEN_UNIT_ID_TEST = "ca-app-pub-3940256099942544/9257395921"
 }
